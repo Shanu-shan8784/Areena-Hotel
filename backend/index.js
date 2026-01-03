c745ea0a2773d03675789c19a22d79cac17bc4e7
Zohaib Jaffar <xohaibshiekh@gmail.com>
2026-01-03 14:06:23 +0500

    Stop tracking ignored files and folders and connect the database with Mongo

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 require("dotenv").config()
 const express = require("express")
+const MongoDBConnetion = require("./Connection/connection.js")
 
+const User = require('./Models/user.js'); // path to your schema file
 
+async function createUser() {
+    try {
+        const newUser = new User({ name: "John Doe" });
+        await newUser.save();
+        console.log("User saved! Check your MongoDB Atlas now.");
+    } catch (err) {
+        console.error("Error saving user:", err);
+    }
+}
 
+MongoDBConnetion(process.env.Mongo_url)
+
+createUser()
 
 const app = express()
 PORT = process.env.PORT||8009
