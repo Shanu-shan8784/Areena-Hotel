ec6be3410bd5df53c66ad2a3a0d73aafd97af489
Zohaib Jaffar <xohaibshiekh@gmail.com>
2026-01-03 12:07:20 +0500

    Set routes and delete unneccessaryfloders

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 require("dotenv").config()
 const express = require("express")
-
+const routes = require('./Routes/routes.js')
 
 
 
 const app = express()
 PORT = process.env.PORT||8009
 
+
+
+app.use("/",routes)
 
 
 
